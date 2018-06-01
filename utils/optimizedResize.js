1bcb9a462e58218815d30b9cb6afeb0415dd8d71
Ahmed Elhanafy <ahmed.elhanafy95@gmail.com>
2018-06-01 20:31:34 +0200

    :tada: Initial Commit

---
+++ utils/optimizedResize.js
@@ -0,0 +1,43 @@
+export default (() => {
+  const callbacks = [];
+  let running = false;
+
+  // fired on resize event
+  function resize() {
+    if (!running) {
+      running = true;
+
+      if (window.requestAnimationFrame) {
+        window.requestAnimationFrame(runCallbacks);
+      } else {
+        setTimeout(runCallbacks, 66);
+      }
+    }
+  }
+
+  // run the actual callbacks
+  function runCallbacks() {
+    callbacks.forEach(function(callback) {
+      callback();
+    });
+
+    running = false;
+  }
+
+  // adds callback to loop
+  function addCallback(callback) {
+    if (callback) {
+      callbacks.push(callback);
+    }
+  }
+
+  return {
+    // public method to add additional callback
+    subscribe: function(callback) {
+      if (!callbacks.length) {
+        window.addEventListener('resize', resize);
+      }
+      addCallback(callback);
+    },
+  };
+})();
